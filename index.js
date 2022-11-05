9dd2b13ab23cf04460f6146281b2044ba7679d99
MaryTreasure <mari1991918@gmail.com>
2022-11-05 13:48:58 +0300

    Условие if

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Привет!')
+const age = 15;
+const gender = 'ЖЕН'
+
+if (gender.toLowerCase() === 'муж') {
+    console.log('Вам налево');
+}
+
+if (gender.toLowerCase() === 'жен') {
+    console.log('Вам направо');
+}
