99304c00e84fcb48f51fe0350e18e35bee59e622
MaryTreasure <mari1991918@gmail.com>
2022-11-05 13:53:22 +0300

    Добавили else в условие проверки возраста

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const age = 15;
 const gender = 'ЖЕН'
 
+if (age > 18) {
+    console.log('Вам больше 18 лет');
+} else {
+    console.log ("Вам меньше 18 лет");
+}
+
 if (gender.toLowerCase() === 'муж') {
     console.log('Вам налево');
-}
+} 
 
 if (gender.toLowerCase() === 'жен') {
     console.log('Вам направо');
