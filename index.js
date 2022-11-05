7e28833c33dd9a8494d4a1b11856a68269663a1e
MaryTreasure <mari1991918@gmail.com>
2022-11-05 14:05:51 +0300

    Добавила условие при отсутсвии возраста

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-const age = 15;
-const gender = 'ЖЕН'
+let age;
+
+const gender = 'ЖЕН';
 
 if (age > 18) {
     console.log('Вам больше 18 лет');
+} else if (age === 18) {
+    console.log("Вам 18 лет");
+} else if (age === undefined) {
+    console.log("Укажите Ваш возраст");
 } else {
     console.log ("Вам меньше 18 лет");
 }
 
+
 if (gender.toLowerCase() === 'муж') {
     console.log('Вам налево');
 } 
+
 
 if (gender.toLowerCase() === 'жен') {
     console.log('Вам направо');
